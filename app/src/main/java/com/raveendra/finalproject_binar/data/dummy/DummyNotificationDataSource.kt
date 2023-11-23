63467a6b11dce71acaa16234ed8e0719cded482c
_bahrulilmi <akunutama3003@gmail.com>
2023-11-23 10:53:34 +0700

    ui and dummyDataNotification

--- app/src/main/java/com/raveendra/finalproject_binar/data/dummy/DummyNotificationDataSource.kt
+++ app/src/main/java/com/raveendra/finalproject_binar/data/dummy/DummyNotificationDataSource.kt
@@ -11,26 +11,29 @@ class DummyNotificationdataSourceImpl() : DummyNotificationDataSource {
     override fun getNotificationData(): List<Notification> {
         return mutableListOf(
             Notification(
+                image = "https://raw.githubusercontent.com/Bahrulilmi30/appFoodYL/master/app/src/main/res/drawable/ic_banner.png",
                 label = "Promo",
                 date = "2 Maret, 12:00",
-//                elipse = "https://raw.githubusercontent.com/Bahrulilmi30/appFoodYL/master/app/src/main/res/drawable/ic_ellipse_green.png",
+                elipse = "https://github.com/Bahrulilmi30/appFoodYL/blob/master/app/src/main/res/drawable/ic_elllipse_green.xml",
                 text = "Dapatkan Potongan 50% selama Bulan Maret!",
                 description = "Syarat dan Ketentuan berlaku!"
 
             ),
 
             Notification(
+                image = "https://github.com/Bahrulilmi30/appFoodYL/blob/master/app/src/main/res/drawable/ic_notification.xml",
                 label = "Notifikasi",
                 date = "1 Maret, 10:00",
-//                elipse = "https://raw.githubusercontent.com/Bahrulilmi30/appFoodYL/master/app/src/main/res/drawable/ic_ellipse_red.png",
+                elipse = "https://github.com/Bahrulilmi30/appFoodYL/blob/master/app/src/main/res/drawable/ic_elllipse_red.xml",
                 text = "Password Berhasil diubah!",
                 description = "",
             ),
 
             Notification(
+                image = "https://github.com/Bahrulilmi30/appFoodYL/blob/master/app/src/main/res/drawable/ic_notification.xml",
                 label = "Notifikasi",
                 date = "15 Maret, 19:00",
-//                elipse = "https://raw.githubusercontent.com/Bahrulilmi30/appFoodYL/master/app/src/main/res/drawable/ic_ellipse_green.png",
+                elipse = "https://github.com/Bahrulilmi30/appFoodYL/blob/master/app/src/main/res/drawable/ic_elllipse_green.xml",
                 text = "Rasakan senasinya mendapatkan potongan 10% pertamamu dan nikmati belajarmu!",
                 description = "Syarat dan Ketentuan berlaku!"
             )
--- app/src/main/java/com/raveendra/finalproject_binar/presentation/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/raveendra/finalproject_binar/presentation/notifications/NotificationsFragment.kt
@@ -7,12 +7,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.raveendra.finalproject_binar.data.dummy.DummyNotificationdataSourceImpl
 import com.raveendra.finalproject_binar.databinding.FragmentAccountBinding
 import com.raveendra.finalproject_binar.databinding.FragmentNotificationsBinding
-import com.raveendra.finalproject_binar.model.Notification
 import com.raveendra.finalproject_binar.presentation.account.AccountViewModel
-import com.raveendra.finalproject_binar.presentation.notifications.adapter.NotificationAdapter
 import com.raveendra.finalproject_binar.utils.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
